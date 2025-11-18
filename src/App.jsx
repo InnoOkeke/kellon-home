0c9887e586bae71bdbeaa09955e9ccc865bc0c84
Rex739 <ojemehprogress@gmail.com>
2025-11-18 22:30:30 +0100

    Refactor: restructure application layout and add new sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Hero from './components/Hero'
-import Partners from './components/Partners'
-import Features from './components/Features'
-import MarketOpportunity from './components/MarketOpportunity'
-import Traction from './components/Traction'
-import About from './components/About'
-import Footer from './components/Footer'
-import PrivacyPolicy from './components/PrivacyPolicy'
-import TermsOfUse from './components/TermsOfUse'
-import Disclaimer from './components/Disclaimer'
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import Partners from "./components/Partners"
+import Features from "./components/Features"
+import MarketOpportunity from "./components/MarketOpportunity"
+import Traction from "./components/Traction"
+import About from "./components/About"
+import Footer from "./components/Footer"
+import PrivacyPolicy from "./components/PrivacyPolicy"
+import TermsOfUse from "./components/TermsOfUse"
+import Disclaimer from "./components/Disclaimer"
+import HowItWorks from "./components/HowItWorks"
+import FAQ from "./components/FAQ"
+import RegulatoryNote from "./components/Regulatory"
+import TokenizedRWA from "./components/TokenizedRWA"
+import ParallaxSection from "./components/ParralaxSection"
 
 // Home page component
 const HomePage = () => (
-  <div className="min-h-screen bg-gradient-to-br from-primary-900 via-accent-900 to-primary-800">
+  <div className="min-h-screen  font-manrope ">
     <Header />
     <main>
       <Hero />
       {/* <Partners /> */}
       <Features />
+      <HowItWorks />
       <MarketOpportunity />
       <Traction />
+      <ParallaxSection />
       <About />
+      <FAQ />
     </main>
     <Footer />
   </div>
